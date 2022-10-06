359ecfe977fc53be13bf49754eb308d6c5f15051
Ahmadreza Yousefi <ah.youmk@gmail.com>
2022-10-06 13:57:51 +0330

    Added curve (hmmmm)

--- frontend/cein-web-taskmanager/src/components/Login/components/Layout.jsx
+++ frontend/cein-web-taskmanager/src/components/Login/components/Layout.jsx
@@ -2,8 +2,13 @@ import '../styles/layout.css';
 
 function Layout() {
   return (
-    <div className="container">
-      <h1></h1>
+    <div className="container spacer wave">
+      <div className="layout-login__left">
+        <h1>hello</h1>
+      </div>
+      <div className="layout-login__right">
+        <h1>hello</h1>
+      </div>
     </div>
   );
 }
