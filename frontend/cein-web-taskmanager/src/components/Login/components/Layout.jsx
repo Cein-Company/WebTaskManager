2cc0bc72ac79fbf6d449921b7e6cc12528b596e3
Ahmadreza Yousefi <ah.youmk@gmail.com>
2022-10-07 09:05:44 +0330

    Styled left part

--- frontend/cein-web-taskmanager/src/components/Login/components/Layout.jsx
+++ frontend/cein-web-taskmanager/src/components/Login/components/Layout.jsx
@@ -4,10 +4,21 @@ function Layout() {
   return (
     <div className="container spacer wave">
       <div className="layout-login__left">
-        <h1>hello</h1>
+        <div className="layout-login__left__text">
+          <p>
+            CEIN <br /> Task Manager
+          </p>
+          <span>Don't have an account? Sign up!</span>
+        </div>
+        <div className="layout-login__left__button">
+          <a href="#">Register</a>
+        </div>
       </div>
       <div className="layout-login__right">
-        <h1>hello</h1>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
