c1884d59115c05b24c52da5c292cbdbf94194af3
Ahmadreza Yousefi <ah.youmk@gmail.com>
2022-10-07 10:10:17 +0330

    Finalize styling

--- frontend/cein-web-taskmanager/src/components/Login/components/Layout.jsx
+++ frontend/cein-web-taskmanager/src/components/Login/components/Layout.jsx
@@ -1,6 +1,10 @@
 import '../styles/layout.css';
 
 function Layout() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container spacer wave">
       <div className="layout-login__left">
@@ -19,17 +23,18 @@ function Layout() {
           <h1>Login</h1>
         </div>
         <div className="layout-login__right__inputs">
-          <input id="username" type="text" placeholder="Username" />
-          <input id="password" type="password" placeholder="Password" />
+          <form onSubmit={submitHandler}>
+            <input id="username" type="text" placeholder="Username" />
+            <input id="password" type="password" placeholder="Password" />
+            <button type="submit">Submit</button>
+          </form>
         </div>
         <div className="layout-login__right__forgot">
           <p>
             Forgot your password? <a href="#">Click here</a>
           </p>
         </div>
-        <div className="layout-login__right__button">
-          <button>Submit</button>
-        </div>
+        <div className="layout-login__right__button"></div>
       </div>
     </div>
   );
